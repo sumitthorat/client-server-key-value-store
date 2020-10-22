80123fd245af95c36ca388bb603cce4d65c3a619
Sumit Thorat <sumit.thorat97@gmail.com>
2020-10-22 23:19:13 +0530

    Add epoll() functionality and dummy client for testing

---
+++ client.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <netdb.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int SERVER_PORT;
+long CACHE_LEN;
+int NUM_WORKER_THREADS;
+
+void connect_send();
+void read_config();
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char** argv) {
+    // Read the config file
+    read_config();
+
+    int np = 8;
+    // "np" clients
+    for (int i = 0; i < np; ++i) {
+        if (fork() == 0) {
+            connect_send();
+            exit(0);
+        }
+    }
+    
+    return 0;
+}
+
+void connect_send() {
+    int sockfd, n;
+    struct sockaddr_in serv_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        error("ERROR opening socket");
+    }
+        
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_port = htons(SERVER_PORT);
+
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        error("ERROR connecting");
+    }
+    
+    int pid = getpid();
+    char mypid[6];   // ex. 34567
+    sprintf(mypid, "%d", pid);
+
+
+    int max = 5, count = 0;
+    char* buff; 
+    while (count < max) {
+        char buff[12] = "Hello";
+        strcat(buff, mypid);
+        printf("%s\n", buff);
+
+        n = write(sockfd, buff, strlen(buff));
+        if (n < 0) {
+            error("Error writing to socket");
+        }
+        sleep(3);
+        count++;
+    }
+
+    close(sockfd);
+
+    printf("Done...\n");
+}
+
+void read_config() {
+    // Open config file
+    FILE* fptr = fopen("config.txt", "r");
+    if (fptr == NULL) {
+        error("Could not open config file");
+    }
+
+    /* 
+        Read info from config file
+        1. Server listening port
+        2. No. of worker threads (n)
+        3. Maximum entries in the cache
+    */
+
+    char buff[256];
+    int buff_len = 256;
+    // TODO: Handle error if required
+    if (fgets(buff, buff_len, fptr) != NULL) {
+        SERVER_PORT = atoi(buff);
+    }
+
+    if (fgets(buff, buff_len, fptr) != NULL) {
+        NUM_WORKER_THREADS = atoi(buff);
+    }
+
+    if (fgets(buff, buff_len, fptr) != NULL) {
+        CACHE_LEN = atol(buff);
+    }
+
+    fclose(fptr);
+}
--- server.c
+++ server.c
@@ -1,22 +1,146 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <signal.h>
+
 
 int SERVER_PORT;
 long CACHE_LEN;
 int NUM_WORKER_THREADS;
+int* worker_epoll_fds;
+int sockfd;
 
 void read_config();
+void* worker(void*);
 
 void error (char* msg) {
     perror(msg);
     exit(1);
 }
 
+
+
+void signal_handler(int sig) {
+    close(sockfd);
+    exit(0);
+}
+
+
+
 int main(int argc, char** argv) {
+    // Register signal handler
+    signal(SIGINT, signal_handler); // for ctrl-c
+
+    // Read config file
     read_config();
+
+    // Setup server's listening socket
+    
+    struct sockaddr_in serv_addr, cli_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        error("Error in server listening socket");
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(SERVER_PORT);
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        error("Error in binding");
+    }
+
+    listen(sockfd, 5);
+
+    /*
+        Setup the data structures for each worker thread i.e. epoll instance specific to each thread
+        epoll_create for n threads, add to array
+    */
+    worker_epoll_fds = (int*) malloc(sizeof(int) * NUM_WORKER_THREADS);
+    int t_ids[NUM_WORKER_THREADS];
+    pthread_t ids[NUM_WORKER_THREADS];
+    for (int i = 0; i < NUM_WORKER_THREADS; ++i) {
+        // Create epoll instance for each thread 
+        worker_epoll_fds[i] = epoll_create1(0);
+        if (worker_epoll_fds[i] == -1) {
+            error("Failed to create epoll instance");
+        }
+
+        // Create and spawn worker thread
+		t_ids[i] = i;
+		pthread_create(&ids[i], NULL, worker, (void*) &t_ids[i]);
+	
+    }
+
+
+    // Conitnuously accept new connectionsd
+    int client_no = 0;
+    struct epoll_event ev;
+    while (1) {
+        // Accept incoming connection
+        int clilen = sizeof(cli_addr);
+        ev.data.fd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        printf("Accepted new connection: %d, assigned to WT = %d\n", cli_addr.sin_port, client_no % NUM_WORKER_THREADS);
+        
+        // Set event listener and assign to appropriate worker thread 
+        ev.events = EPOLLIN;
+        if (epoll_ctl(worker_epoll_fds[client_no % NUM_WORKER_THREADS], EPOLL_CTL_ADD, ev.data.fd, &ev) < 0) {
+            error("Error in epoll adding");
+        }
+
+        client_no++;
+    }
+
+
+
+    for (int i = 0; i < NUM_WORKER_THREADS; ++i) {
+        pthread_join(ids[i], NULL);
+        close(worker_epoll_fds[i]);
+    }
     
     return 0;
+}
+
+void* worker(void* arg) {
+    int id = *((int*)arg);
+
+    printf("Thread %d is ready\n", id);
+
+
+    // // Probe the file descriptors
+    struct epoll_event events[8];
+    
+    while (1) {
+        printf("New round\n");
+        int nfds = epoll_wait(worker_epoll_fds[id], events, 8, 10000);
+
+        char buff[11];
+        int buff_len = 11;
+        for (int i = 0; i < nfds; ++i) {
+            memset(buff, 0, buff_len);
+            ssize_t len = read(events[i].data.fd, buff, buff_len);
+            if (len == 0) {
+                close(events[i].data.fd);
+                continue;
+            }
+
+            if (len < 0) {
+                error("Read Error");
+            }
+
+            
+            printf("WT = %d, MSG = %s\n", id, buff);
+            // Here request will be parsed and appropriate action will be taken
+        }
+    }
 }
 
 void read_config() {
@@ -50,3 +174,4 @@ void read_config() {
 
     fclose(fptr);
 }
+
